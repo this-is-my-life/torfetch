febae38794839ad21162b8aa87ec8b90ada2d3f5
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2020-10-05 21:11:45 +0900

    nice.

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 const torRequest = require('torrequest')
 const express = require('express')
+const cheerio = require('cheerio')
 const app = express()
 
 const log = []
 
 app.get('/', (req, res, _) => {
   if (!req.query.url) return res.send('usage: /?url=http://example.org')
+  console.log('Request: ', req.query.url)
   torRequest({
     uri: req.query.url,
     torHost: 'localhost',
     torPort: 9050
   }, (err, rep) => {
     if (err) return res.send(err)
-    res.send(rep.body)
-    log.push({ ip: req.ip, url: new URL(req.query.url) })
+    console.log('Response: length=', rep.body.length)
+
+    const $ = cheerio.load(rep.body)
+    const url = new URL(req.query.url)
+
+    $('a').each((i, e) => {
+      if (!e.attribs.href) return
+      if (e.attribs.href.startsWith('#')) return
+      if (e.attribs.href.startsWith('/')) return ($('a')[i].attribs.href = '/?url=' + encodeURIComponent(url.protocol + url.host + e.attribs.href))
+      $('a')[i].attribs.href = '/?url=' + encodeURIComponent(e.attribs.href)
+    })
+
+    res.send($.html())
+    log.push({ ip: req.ip, url })
   })
 })
 
-app.use((req, res) => {
-  if (req.path === '/') return
-  const index = log.findIndex((l) => l.ip === req.ip)
-  if (index < 0) return res.redirect('/')
-
-  log[index].url.pathname = req.path
-  console.log(log[index])
-
-  res.redirect('/?url=' + encodeURIComponent(log[index].url.href))
-  log.splice(index, 1)
-})
 app.listen(8080)
